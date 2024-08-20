7cb13691b6aafbc8a3664f6b960afb0609ba58c3
JacobB9990 <jacobb9990@gmail.com>
2024-08-20 10:38:26 -0400

    The theme should stay when you change pages

--- js/toggle-theme.js
+++ js/toggle-theme.js
@@ -1,20 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
     function initThemeToggle() {
         const themeToggleButton = document.getElementById('theme-toggle');
+        const stylesheet = document.getElementById('theme-stylesheet');
+        const currentTheme = localStorage.getItem('theme');
+
+        if (currentTheme) {
+            stylesheet.setAttribute('href', `/css/${currentTheme}.css`);
+        }
+
         if (themeToggleButton) {
             themeToggleButton.addEventListener('click', () => {
-                const stylesheet = document.getElementById('theme-stylesheet');
                 const currentTheme = stylesheet.getAttribute('href');
 
                 if (currentTheme.includes('style-dark.css')) {
                     stylesheet.setAttribute('href', '/css/style-light.css');
+                    localStorage.setItem('theme', 'style-light');
                 } else {
                     stylesheet.setAttribute('href', '/css/style-dark.css');
+                    localStorage.setItem('theme', 'style-dark');
                 }
             });
         }
     }
-
 
     const observer = new MutationObserver(() => {
         const header = document.querySelector('header');
